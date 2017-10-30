421524608ab8d033cd2e22dc1ceddf5309a596fd
Phodal Huang <h@phodal.com>
2017-10-30 16:13:35 +0800

    [T] add aws-serverless-express for rendering

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
@@ -6,4 +5,4 @@ app.get('/', function (req, res) {
       res.send('Hello World!')
 })
 
-module.exports.handler = serverless(app);
+module.exports = app;
